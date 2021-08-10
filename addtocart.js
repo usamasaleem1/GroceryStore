2814ff900a9df488dc46c8f0139b2359d903a949
scott <scottjamessmith05@gmail.com>
2021-08-10 13:04:58 -0400

    local storage for quantity selection

--- addtocart.js
+++ addtocart.js
@@ -3,19 +3,28 @@ addToCart = document.getElementById("addToCartButton");
 price = document.getElementById("price");
 titleName = document.getElementsByTagName("title")[0].innerText;
 
+loadedStorage = false;
+
 function updateCartPrice()
 {
-    if (typeof(Storage) !== "undefined") 
+    if (localStorage.getItem(String(titleName))) 
     {
-        // Store
-        sessionStorage.setItem(titleName, Number(inputElement.value));
-        // Retrieve
-        addToCart.innerText = "$" + (Number(sessionStorage.getItem(titleName)) * Number(price.innerHTML.replace("$", ""))).toFixed(2) + " Add To Cart";
-
-        console.log(sessionStorage.getItem(titleName));
+        if(!loadedStorage)
+        {
+            addToCart.innerText = "$" + (Number(localStorage.getItem(String(titleName))) * Number(price.innerHTML.replace("$", ""))).toFixed(2) + " Add To Cart";
+            inputElement.value = Number(localStorage.getItem(String(titleName)));
+            loadedStorage = true;
+        }
+        else
+        {
+            localStorage.setItem(String(titleName), String(inputElement.value));
+            addToCart.innerText = "$" + (Number(localStorage.getItem(String(titleName))) * Number(price.innerHTML.replace("$", ""))).toFixed(2) + " Add To Cart";
+            console.log(localStorage.getItem(String(titleName)));
+        }
     } 
     else 
     {
-        alert("Sorry, your browser does not support Web Storage...");
+        addToCart.innerText = "$" + (Number(inputElement.value) * Number(price.innerHTML.replace("$", ""))).toFixed(2) + " Add To Cart";
+        localStorage.setItem(String(titleName), String(inputElement.value));
     }
 }
