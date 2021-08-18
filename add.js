31e78672ce9e738ce70af1bb3ac242cb895792c3
Golden-skull <87375351+Golden-skull@users.noreply.github.com>
2021-08-18 13:30:38 -0400

    Delete add.js

--- add.js
+++
@@ -1,8 +0,0 @@
- var carts = document.querySelectorAll('#addToCartButton');
-
- for(var i = 0 ;  i < carts.length; i++){
-
- 	carts[i].addEventListener("click", function(){ prompt("added to cart"); 	});
- 
- } 
- 
