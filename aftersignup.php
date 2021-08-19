ebd2c6abc7198a5b136688b8560c25518991cd03
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 14:57:56 -0400

    updated styling

--- aftersignup.php
+++ aftersignup.php
@@ -47,7 +47,7 @@ else{
 	<meta charset="utf-8">
 	<title>Online Grocery</title>
 	<link rel="icon" href="trolley.png">
-	<link rel="stylesheet" href="home.css">
+	<link rel="stylesheet" href="aftersignup.css">
 	
 <!-- general -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
