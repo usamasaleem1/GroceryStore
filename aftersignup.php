909d910da31dbc80fd89ccc49020e1f0c779536c
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 13:53:49 -0400

    Update aftersignup.php

--- aftersignup.php
+++ aftersignup.php
@@ -12,72 +12,45 @@
 
 
       <?php
-// //connecting to db	  
-// $host = 'remotemysql.com:3306';
-// $db = 'HTADFpjYkD';
-// $user = 'HTADFpjYkD';
-// $pass = 'Ng7fU9bD9m'
-// $charset = 'utf8mb4';
-
-// $conn = new mysqli($host,$user,$pass,$db); 
-// if ($conn-> connect_errno) { 
-//     trigger_error('Database connection failed: ' . $conn->connect_error); 
-//     }
-
-// if ($conn)
-// {
-// 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-// 	$firstname = $_POST['firstname'];	
-// 	$email = $_POST['email'];
-// 	$password = $_POST['password'];
-// 	$address = $_POST['address'];
-// 	$postal = $_POST['postal'];
-
-// //$query = "insert into member values ('" . $_POST["name"] . "' where employee_id = ". $_POST["idz"];
-// 	$q2="INSERT INTO member(firstname,email,password,address,postal)	values ('$firstname',,'$email','$password','$address','$postal')";
-// 	echo $q2;
-	
-// 	 $query_id = oci_parse($conn, $q2); 		
-// 	 $r = oci_execute($query_id);
-			
-// 		}
-// 	}
-// else{
-// 	echo "could not connect";
-// }
-
-
-session_start();
-
 //connecting to db	  
 $host = 'remotemysql.com:3306';
 $db = 'HTADFpjYkD';
 $user = 'HTADFpjYkD';
 $pass = 'Ng7fU9bD9m'
 $charset = 'utf8mb4';
-$db = mysqli_connect("host","user","pass","db");
 
-if(!$db){
-    die("connection error...".mysqli_connect_error());
-}else{
-    echo "You are successfully connected.";
-}
+$conn = new mysqli($host,$user,$pass,$db); 
+if ($conn-> connect_errno) { 
+    trigger_error('Database connection failed: ' . $conn->connect_error); 
+    }
 
-if(isset($_POST['username']) && isset($_POST['password'])){
+if ($conn)
+{
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$firstname = $_POST['firstname'];	
-    $email = $_POST['email'];
- 	$password = $_POST['password'];
- 	$address = $_POST['address'];
- 	$postal = $_POST['postal'];
-    
-$temp = mysqli_query($db,"INSERT INTO registration (username,email,password) 
-VALUES ('$firstname','$email','$password','$address','$postal')");
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$address = $_POST['address'];
+	$postal = $_POST['postal'];
 
-if(!$temp){
-    echo "error";
-}else{
-    echo "Your registration is done.";
-}
+//$query = "insert into member values ('" . $_POST["name"] . "' where employee_id = ". $_POST["idz"];
+$query = "
+
+INSERT INTO `HTADFpjYkD` `register` (`firstname`, `email`,
+`password`, `address`, `postal`) VALUES ('$firstname', '$email', '$password', '$address', '$postal'); ";
+
+mysql_query($query);
+
+echo "
+
+Thanks for getting in touch.
+
+";
+			
+		}
+	}
+else{
+	echo "could not connect";
 }
 
 
