0133f81458fefb70730f9cfdcd34efde8797897c
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 14:48:15 -0400

    Update aftersignup.php

--- aftersignup.php
+++ aftersignup.php
@@ -1,14 +1,65 @@
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>user_dashboard</title>
-  </head>
-  <body>
-    <p style="text-align: center;"><u><b>Sign up Succesfull!</b></u></p>
-    <p style="text-align: center;"><u><b><br>
-        </b></u></p>
-    <p>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="project.css">
+	<link rel="stylesheet" type="text/css" href="signinup.css">
+	
+	<meta charset="utf-8">
+	<title>Sign Up</title>
+	<link rel="icon" href="trolley.png">
+
+</head>
+<body>
+
+
+<!-- Nav bar -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	<img src="trolley.png" class="navbar-brand" style="width: 40px;">
+	<a class="navbar-brand" href="#">Online Grocery Shopping</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	<ul class="navbar-nav mr-auto mx-auto">
+	  <li class="nav-item active">
+		<a class="nav-link" href="home.html">Home <span class="sr-only"></span></a>
+	  </li>
+	  <li class="nav-item active">
+		<a class="nav-link" href="AllProducts.html">All Products</a>
+	  </li>
+	  <li class="nav-item dropdown active">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		  Aisles
+		</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		  <a class="dropdown-item" href="meataisle.html">Meat</a>
+		  <a id="disabled" class="dropdown-item" href="dairy.html">Dairy</a>  
+		  <a id="disabled" class="dropdown-item" href="beveragesaisle.html">Beverages</a>
+		  <a id="disabled" class="dropdown-item" href="detergents.html">Detergents</a>
+		<a id="disabled" class="dropdown-item" href="snacks.html">Snacks</a>
+		  <a id="disabled" class="dropdown-item" href="alcohol.html">Alcohol</a>
+		</div>
+	  </li>
+	  <li class="nav-item active">
+		<a class="nav-link disabled" href="contact.html">Contact Us</a>
+	  </li>
+	</ul>
+	<br>
+	<form class="form-inline my-2 my-lg-0">
+	  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="background-color: #343A40;">
+	  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+	  <a class="btn btn-outline-primary ml-1" href="#">Sign in</a>
+	</form>
+  </div>
+</nav>
 
 
 
@@ -62,7 +113,3 @@ else{
 echo " step4";
 ?>
 
-
-    </p>
-  </body>
-</html>
