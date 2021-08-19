a20b406637671b793ace8c0a426798cf622633ce
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 14:02:25 -0400

    Update aftersignup.php

--- aftersignup.php
+++ aftersignup.php
@@ -12,42 +12,60 @@
 
 
       <?php
-//connecting to db	  
-$host = 'remotemysql.com:3306';
-$db = 'HTADFpjYkD';
-$user = 'HTADFpjYkD';
-$pass = 'Ng7fU9bD9m'
-$charset = 'utf8mb4';
 
-$conn = new mysqli($host,$user,$pass,$db); 
-if ($conn-> connect_errno) { 
-    trigger_error('Database connection failed: ' . $conn->connect_error); 
-    }
+$dbhost = 'remotemysql.com:3306';
+$dbuser = 'HTADFpjYkD';
+$dbpass = 'Ng7fU9bD9m';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
-if ($conn)
-{
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$firstname = $_POST['firstname'];	
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$address = $_POST['address'];
-	$postal = $_POST['postal'];
-
-//$query = "insert into member values ('" . $_POST["name"] . "' where employee_id = ". $_POST["idz"];
-$query = "
-
-INSERT INTO 'HTADFpjYkD' 'register' ('firstname', 'email',
-'password', 'address', 'postal') VALUES ('$firstname', '$email', '$password', '$address', '$postal'); ";
-
-mysql_query($query);
-
-echo "Thanks for getting in touch.";
-			
-		}
-	}
-else{
-	echo "could not connect";
+if(! $conn ) {
+   die('Could not connect: ' . mysqli_error());
 }
+echo 'Connected successfully';
+mysqli_close($conn);
+
+
+
+
+
+
+
+// //connecting to db	  
+// $host = 'remotemysql.com:3306';
+// $db = 'HTADFpjYkD';
+// $user = 'HTADFpjYkD';
+// $pass = 'Ng7fU9bD9m'
+// $charset = 'utf8mb4';
+
+// $conn = new mysqli($host,$user,$pass,$db); 
+// if ($conn-> connect_errno) { 
+//     trigger_error('Database connection failed: ' . $conn->connect_error); 
+//     }
+
+// if ($conn)
+// {
+// 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// 	$firstname = $_POST['firstname'];	
+// 	$email = $_POST['email'];
+// 	$password = $_POST['password'];
+// 	$address = $_POST['address'];
+// 	$postal = $_POST['postal'];
+
+// //$query = "insert into member values ('" . $_POST["name"] . "' where employee_id = ". $_POST["idz"];
+// $query = "
+
+// INSERT INTO 'HTADFpjYkD' 'register' ('firstname', 'email',
+// 'password', 'address', 'postal') VALUES ('$firstname', '$email', '$password', '$address', '$postal'); ";
+
+// mysql_query($query);
+
+// echo "Thanks for getting in touch.";
+			
+// 		}
+// 	}
+// else{
+// 	echo "could not connect";
+// }
 
 
 ?>
