6904ca0ccfce82d79bc197db470b08645596ffbd
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 20:08:43 -0400

    Update aftersignup.php

--- aftersignup.php
+++ aftersignup.php
@@ -26,7 +26,6 @@ VALUES ('$firstname', '$email', '$password', '$address', '$postal')";
 
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
