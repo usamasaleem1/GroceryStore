8ecd92024cc552cd7ffc4989c78702b06816610f
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 22:48:48 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,3 +3,14 @@ session_start();
 session_destroy();
 header('location:index.php');
 ?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Meta Tag</title>
+      <meta http-equiv = "refresh" content = "0; url = https://concordiagrocery.herokuapp.com/home.html" />
+   </head>
+   <body>
+      <p>loading...</p>
+   </body>
+</html>
