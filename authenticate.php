255e18024a8f0c1270cb88fcbbc07b0660af1331
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-20 00:53:20 -0400

    test

--- authenticate.php
+++ authenticate.php
@@ -16,11 +16,11 @@
 					$_SESSION['loggedin'] = true;	
 					$_SESSION['email']=$row['email'];
 					if ($password == "admin" && $email == "admin"){
-						$_SESSION['permission'] == 'admin';
+						$_SESSION['permission'] = 'admin';
 						header('location:backstore.php');
 					} else {
 						$_SESSION['permission'] = 'user';
-						header('location:home.php');
+						header('location:contact.php');
 					}
 					
 				}
