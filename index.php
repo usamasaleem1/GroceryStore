20e0730beee08ea7af05a6cd10aeee97a1aab7b9
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 21:52:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php include_once("home.html"); ?>
+<?php session_start(); ?>
+<?php include('dbcon.php'); ?>
+<?php
+	if (isset($_POST['submit']))
+		{
+			$email = mysqli_real_escape_string($conn, $_POST['email']);
+			$password = mysqli_real_escape_string($conn, $_POST['password']);
+			
+			$query 		= mysqli_query($conn, "SELECT * FROM register WHERE  password='$password' and email='$email'");
+			$row		= mysqli_fetch_array($query);
+			$num_row 	= mysqli_num_rows($query);
+			
+			if ($num_row > 0) 
+				{			
+					$_SESSION['email']=$row['email'];
+					header('location:home.html');
+					
+				}
+			else
+				{
+					echo 'Invalid email and Password Combination';
+				}
+		}
+  ?>
