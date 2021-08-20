60ab505c2867f83e817fd2a7fb9c64effce71f8c
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-19 22:03:22 -0400

    Update backstore.php

--- backstore.php
+++ backstore.php
@@ -1,14 +1,13 @@
 <?php 
 include ('index.php');
 include('dbcon.php'); 
-$_SESSION['email']=$row['email'];
+
 if ($password == "admin" && $email == "admin"){
     header('location:backstore.php');
 } else {
     header('location:home.html');
 }
 
-$session_id=$_SESSION['email'];
 
 
 ?>
