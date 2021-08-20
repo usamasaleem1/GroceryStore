f157d1af2e4c4b4e1620c7388455c9214255abe0
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-20 00:37:13 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
-
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +11,6 @@
 	
 <!-- general -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -19,53 +19,53 @@
 	<link rel="icon" href="trolley.png">
 </head>
 <body>
-
 <!-- Nav bar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 	<img src="trolley.png" class="navbar-brand" style="width: 40px;">
 	<a class="navbar-brand" href="#">Online Grocery Shopping</a>
-
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	<ul class="navbar-nav mr-auto mx-auto">
 	  <li class="nav-item active">
-		<a class="nav-link" href="home.php">Home <span class="sr-only"></span></a>
+		<a class="nav-link" href="home.html">Home <span class="sr-only"></span></a>
 	  </li>
 	  <li class="nav-item active">
-		<a class="nav-link" href="AllProducts.php">All Products</a>
+		<a class="nav-link" href="AllProducts.html">All Products</a>
 	  </li>
 	  <li class="nav-item dropdown active">
 		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 		  Aisles
 		</a>
 		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		  <a class="dropdown-item" href="meataisle.php">Meat</a>
-		  <a id="disabled" class="dropdown-item" href="dairy.php">Dairy</a>  
-		  <a id="disabled" class="dropdown-item" href="beveragesaisle.php">Beverages</a>
-		  <a id="disabled" class="dropdown-item" href="detergents.php">Detergents</a>
-		<a id="disabled" class="dropdown-item" href="snacks.php">Snacks</a>
-		  <a id="disabled" class="dropdown-item" href="alcohol.php">Alcohol</a>
+		  <a class="dropdown-item" href="meataisle.html">Meat</a>
+		  <a id="disabled" class="dropdown-item" href="dairy.html">Dairy</a>  
+		  <a id="disabled" class="dropdown-item" href="beveragesaisle.html">Beverages</a>
+		  <a id="disabled" class="dropdown-item" href="detergents.html">Detergents</a>
+		<a id="disabled" class="dropdown-item" href="snacks.html">Snacks</a>
+		  <a id="disabled" class="dropdown-item" href="alcohol.html">Alcohol</a>
 		</div>
 	  </li>
 	  <li class="nav-item active">
-		<a class="nav-link disabled" href="contact.php">Contact Us</a>
+		<a class="nav-link disabled" href="contact.html">Contact Us</a>
 	  </li>
 	</ul>
 	<br>
 	<form class="form-inline my-2 my-lg-0">
 	  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="background-color: #343A40;">
 	  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		<a class="btn btn-outline-primary ml-1" href="signinpage.php">Sign in</a>
+
+      <?php if(isset($_SESSION['loggedin'])){ ?>
+        <a class="btn btn-outline-primary ml-1" href="logout.php">Log Out</a>
+    <?php }else{ ?>
+        <a class="btn btn-outline-primary ml-1" href="signinpage.php">Sign in</a>
+    <?php } ?>
+      
 	</form>
   </div>
 </nav>
-
-
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
 	<div class="bgimg">
 		<!-- <div class="topmiddle">
 		  <p><img src="trolley.png" style="width: 100px;"></p>
@@ -79,8 +79,6 @@
 		  <p>Some text</p>
 		</div> -->
 	  </div>
-
-
 
 </body>
 </html>
