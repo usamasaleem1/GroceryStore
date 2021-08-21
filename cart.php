b2ecb6c026a6ad6484d908284e3c2d983b7d83ed
Golden-skull <87375351+Golden-skull@users.noreply.github.com>
2021-08-21 14:28:44 -0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -9,10 +9,10 @@ $connect = mysqli_connect('remotemysql.com:3306', 'HTADFpjYkD', 'wfJDJmJgdL', 'H
 if (isset($_POST['add'])) {
   if (isset($_SESSION['cart'])) {
     $item_array_id = array_column($_SESSION['cart'], "product_id");
-    if (!in_array($_POST['id'], $item_array_id)) {
+    if (!in_array($_GET['id'], $item_array_id)) {
       $count = count($_SESSION['cart']);
       $item_array = array(
-        "product_id" => $_POST['id'],
+        "product_id" => $_GET['id'],
         "item_name" => $_POST['hidden_name'],
         "product_price" => $_POST["hidden_price"],
         "item_quantity" => $_POST['quantity']
