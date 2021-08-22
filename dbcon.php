17814eda8b442933ec60d426d5c92e2d85bef09e
alessioamo <alessio.amorelli@hotmail.com>
2021-08-21 23:08:43 -0400

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -4,6 +4,7 @@ $dbhost = 'remotemysql.com:3306';
 $dbuser = 'HTADFpjYkD';
 $dbpass = 'wfJDJmJgdL';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbuser);
+mysqli_select_db($conn,"HTADFpjYkD");
 
 // Check connection
 if (mysqli_connect_errno())
