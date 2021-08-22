e2abe9deadf5ee52b2f732479af82b43f83ed7cd
alessioamo <alessio.amorelli@hotmail.com>
2021-08-22 01:31:23 -0400

    Update backstoreEdit.php

--- backstoreEdit.php
+++ backstoreEdit.php
@@ -160,7 +160,7 @@ header('location:home.php');
 
 	mysql_select_db("HTADFpjYkD",$conn);
 
-	$sql = "INSERT INTO test (name,price,description) VALUES ('$_POST[name]','$_POST[price]','$_POST[decription]')";
+	$sql = "INSERT INTO test (name,price,description) VALUES ('$_POST[name]','$_POST[price]','$_POST[description]')";
 
 	mysql_query($sql,$conn);
 
