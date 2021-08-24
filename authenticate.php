0b5af5cf47d97b1cad7a6e871a889d795fb93852
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-24 18:15:08 -0400

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -10,7 +10,9 @@
 			$query 		= mysqli_query($conn, "SELECT * FROM register WHERE password='$password' and email='$email'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
-
+			
+			$firstname = $_SESSION['firstname'];
+			
 			$query2 	= mysqli_query($conn, "SELECT firstname FROM register");
 			$row2		= mysqli_fetch_array($query2);
 			$num_row2 	= mysqli_num_rows($query2);
@@ -33,7 +35,6 @@
 					$_SESSION['loggedin'] = false;
 					header('location:signinpageError.php');
 				}
-			$firstname = $_SESSION['firstname'];
 		
 		}
 
