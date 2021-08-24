31b9530fffcb5cadb69172c65e064109016e1d3b
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-24 18:22:27 -0400

    Update home.php

--- home.php
+++ home.php
@@ -31,7 +31,7 @@ $result = mysqli_query($conn,"SELECT firstname FROM register");
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 	<?php if(isset($_SESSION['loggedin'])){ ?>
 				<img src="iconLoggedIn.png" class="navbar-brand" style="width: 30px;">
-				<a class="navbar-brand" href="#"><?php echo $_SESSION["firstname"]; ?></a>
+				<a class="navbar-brand" href="#"><?php echo "Welcome, ".$_SESSION["firstname"]; ?></a>
   			<?php }else{ ?>
 				<img src="trolley.png" class="navbar-brand" style="width: 40px;">
 				<a class="navbar-brand" href="#">Online Grocery Shopping</a>
