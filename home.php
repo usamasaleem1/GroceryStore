bb550ecf58e3c72d3ed3464f36c85efc8e0f46e5
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-24 15:50:48 -0400

    Update home.php

--- home.php
+++ home.php
@@ -30,7 +30,6 @@ session_start();
   			<?php }else{ ?>
 				<a class="navbar-brand" href="#">Online Grocery Shopping</a>
   			<?php } ?>
-	<a class="navbar-brand" href="#">Online Grocery Shopping</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
   </button>
