e5ef306c99365fb86620738b561fbd4e3f2e96c0
scott <scottjamessmith05@gmail.com>
2021-08-24 11:34:37 -0400

    Update backstoreEdit.php

--- backstoreEdit.php
+++ backstoreEdit.php
@@ -101,6 +101,39 @@ header('location:home.php');
 			$id = $_GET['productId'];
 			$sql = "UPDATE products SET name='$name', aisle='$aisle', description='$description', price=$price, stock=$stock  WHERE id=$id";
 
+
+			if(isset($_FILES['image']))
+			{
+				$errors= array();
+				$file_name = $_FILES['image']['name'];
+				$file_size =$_FILES['image']['size'];
+				$file_tmp =$_FILES['image']['tmp_name'];
+				$file_type=$_FILES['image']['type'];
+				$file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+				
+				$extensions= array("jpeg","jpg","png");
+				
+				if(in_array($file_ext,$extensions)=== false)
+				{
+				   $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+				}
+				
+				if($file_size > 2097152)
+				{
+				   $errors[]='File size must be excately 2 MB';
+				}
+				
+				if(empty($errors)==true)
+				{
+				   move_uploaded_file($file_tmp, $file_name);
+				   echo "Success";
+				}
+				else
+				{
+				   print_r($errors);
+				}
+			}
+
 			if (mysqli_query($conn, $sql)) 
 			{
 				echo "Product updated successfully";
@@ -120,7 +153,7 @@ header('location:home.php');
 
 			<h1>Edit Product</h1>
 
-			<form action='backstoreEdit.php?productId=" . $productId . "' method='post'>
+			<form action='backstoreEdit.php?productId=" . $productId . "' method='post' enctype='multipart/form-data'>
 		
 			<div class='form-group row'>
 				<label for='colFormLabel' class='col-sm-2 col-form-label'>Product Name</label>
@@ -155,7 +188,7 @@ header('location:home.php');
 			<div class='custom-file col-md-2'>
 				<label for='customFile' class='col-form-label'>Stock count</label>
 				<label class='custom-file-label' for='customFile'>Image file</label>
-				<input type='file' name='customFile' class='custom-file-input' id='customFile'>
+				<input type='file' name='image' class='custom-file-input' id='customFile'>
 			</div>
 		
 			<div class='form-group row'>
