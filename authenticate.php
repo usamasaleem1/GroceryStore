88d68afc233845e7717374b4f9214286d0e1a2c5
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-24 17:22:33 -0400

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -6,9 +6,8 @@
 		{
 			$email = mysqli_real_escape_string($conn, $_POST['email']);
 			$password = mysqli_real_escape_string($conn, $_POST['password']);
-			$firstname = mysqli_real_escape_string($conn, $_GET['firstname']);;
 
-			$query 		= mysqli_query($conn, "SELECT * FROM register WHERE password='$password' and email='$email' and firstname='$firstname'");
+			$query 		= mysqli_query($conn, "SELECT * FROM register WHERE password='$password' and email='$email'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
