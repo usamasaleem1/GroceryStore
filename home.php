f91932f34732529a587590bb65505eea77b6de93
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-08-24 17:25:40 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 
 <?php include('dbcon.php'); 
 include('authenticate.php');
-$result = mysqli_query($conn,"SELECT * FROM register");
+$result = mysqli_query($conn,"SELECT firstname FROM register");
 
 ?>
 
@@ -31,7 +31,7 @@ $result = mysqli_query($conn,"SELECT * FROM register");
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
 	<?php if(isset($_SESSION['loggedin'])){ ?>
 				<img src="iconLoggedIn.png" class="navbar-brand" style="width: 30px;">
-				<a class="navbar-brand" href="#"><?php echo $_SESSION['firstname']; ?></a>
+				<a class="navbar-brand" href="#"><?php echo $_GET["firstname"]; ?></a>
   			<?php }else{ ?>
 				<img src="trolley.png" class="navbar-brand" style="width: 40px;">
 				<a class="navbar-brand" href="#">Online Grocery Shopping</a>
