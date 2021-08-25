67f2a4ea99b98dbbbf264001bc4ec965dbdbb92e
alessioamo <alessio.amorelli@hotmail.com>
2021-08-25 16:22:29 -0400

    Update backstore.php

--- backstore.php
+++ backstore.php
@@ -80,7 +80,7 @@ header('location:home.php');
 
 <!-- section name -->
 <div class="d-flex flex-column flex-md-row align-items-center p-2 px-md-4 bg-white">
-	<button type='submit' width="140px" height="90px"style="background-color:#343A40; border-radius:30x;" name='userlist'><a href='backstoreUserList.php' style='color:white;'>To User List</a></button>
+	<button type='submit' width="140px" height="90px"style="background-color:#343A40; border-radius:30x;" name='userlist'><a href='backstoreUserList.php' style='color:white; border-radius:30x;'>To User List</a></button>
 	<h5 class="my-0 mr-md-auto font-weight-normal" style="margin:auto;">Backstore</h5>
 	<a class="btn btn-outline-primary" href="backstoreAdd.php">Add <img src="edit.png" width="30px" height="30px"> </a>
   </div>
