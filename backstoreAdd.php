0a686bfdfb24ecdba014c0f2f74ac81de6e9ad6f
alessioamo <alessio.amorelli@hotmail.com>
2021-08-25 01:31:52 -0400

    Update backstoreAdd.php

--- backstoreAdd.php
+++ backstoreAdd.php
@@ -165,7 +165,7 @@ header('location:home.php');
 				<button type='submit' class='btn btn-primary' name='save'>Save</button>
 			</a>
 			
-			<button type='submit' class='btn btn-primary' name='back'><a href="backstore.php">Back</a></button>
+			
 			
 			";
 
