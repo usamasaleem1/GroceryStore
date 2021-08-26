c21f5b2e8d0f77651692e958ad8168737044f9a2
scott <scottjamessmith05@gmail.com>
2021-08-25 21:50:10 -0400

    hide errors

--- dbcon.php
+++ dbcon.php
@@ -13,8 +13,8 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+// error_reporting(E_ALL);
+// ini_set("display_errors", 1);
 ini_set("file_uploads", "On");
 
 ?>
